04f398f8f0433598796271b537c63c81911c0c7c
Sebastian Morr <sebastian@morr.cc>
2012-02-06 09:32:03 +0100

    Titel: git-hydra

--- src/sfml_display.cpp
+++ src/sfml_display.cpp
@@ -9,7 +9,7 @@ class SFMLDisplay {
 
     public:
 
-        SFMLDisplay(Graph& graph) : graph(graph), window(VideoMode(500,500), "Git-Tutor", Style::Default, ContextSettings(0,0,4,3,0)), view(FloatRect(0,0,1000,1000)) {
+        SFMLDisplay(Graph& graph) : graph(graph), window(VideoMode(500,500), "git-hydra", Style::Default, ContextSettings(0,0,4,3,0)), view(FloatRect(0,0,1000,1000)) {
             window.SetView(view);
             //cout << window.GetSettings().AntialiasingLevel << flush;
             font.LoadFromFile(assets_dir()+"/arial.ttf");
